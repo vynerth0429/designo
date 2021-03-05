557a668c8cbf3995cc1a9ae04094e8e0ec107473
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-03-05 17:19:52 +0800

    feat: initial design system definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,35 @@ module.exports = {
       serif: ['sans-serif']
     },
     extend: {
+      fontSize: {
+        'h1': ['3rem', {
+          lineHeight: '3rem',
+          letterSpacing: '0rem',
+        }],
+        'h2': ['2.5rem', {
+          lineHeight: '3rem',
+          letterSpacing: '0.125rem',
+        }],
+        'h3': ['1.25rem', {
+          lineHeight: '1.625rem',
+          letterSpacing: '-0.8px',
+        }],
+        'h4': ['12px', {
+          lineHeight: '15px',
+          letterSpacing: '0.3125rem',
+        }],
+        'body1': ['1rem', {
+          lineHeight: '1.625rem',
+          letterSpacing: '0rem',
+        }],
+      },
+      colors: {
+        'peach': '#E7816B',
+        'peach-light': '#FFAD9B',
+        'dark': '#1D1C1E',
+        'grey-dark': '#333136',
+        'grey-light': '#F1F3F5',
+      },
       container: {
         screens: {
           '2xl': "1280px",
