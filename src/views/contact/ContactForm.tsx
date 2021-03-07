7dbb6cdf953b84ad41ed98fea14545806764c042
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-03-07 12:52:20 +0800

    feat: add form validation

--- src/views/contact/ContactForm.tsx
+++ src/views/contact/ContactForm.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
+import {
+  Formik,
+  Form,
+  FormikProps,
+} from 'formik';
+import * as Yup from 'yup';
+
+const kRequired = "Can't be empty";
+const kInvalidEmail = "Please use a valid email address";
+
+type TContactForm = {
+  [key: string]: any,
+  name: string,
+  email: string,
+  phone: string,
+  message: string,
+}
+
+const InvoiceFormSchema =
+  Yup.object()
+    .shape<TContactForm>({
+      name: Yup.string()
+        .required(kRequired),
+      email: Yup.string()
+        .required(kRequired)
+        .email(kInvalidEmail),
+      phone: Yup.string()
+        .optional(),
+      message: Yup.string()
+        .required(kRequired),
+    });
+
 import useBreakpoints from '../../hooks/useBreakpoints';
 import { ContactData } from '../../data/contact-data';
 
@@ -11,6 +43,7 @@ import ButtonLight from '../button/ButtonLight';
 function ContactForm() {
   const { breakpoint } =  useBreakpoints();
   const [imageBG, setImageBG] = React.useState('');
+  const formRef = React.useRef<FormikProps<TContactForm>>(null);
 
   React.useEffect(() => {
     if (breakpoint === 'xxs') {
@@ -52,37 +85,106 @@ function ContactForm() {
         </div>
       </div>
 
-      <div className="mt-12 md:mt-10 lg:mt-0 lg:flex-1">
-        <div className="space-y-6">
-          <div>
-            <InputComp
-              placeholder="Name"
-            />
-          </div>
-          <div>
-            <InputComp
-              placeholder="Email Address"
-            />
-          </div>
-          <div>
-            <InputComp
-              placeholder="Phone"
-            />
-          </div>
-          <div>
-            <TextAreaComp
-              placeholder="Your Message"
-              rows={5}
-            />
-          </div>
-        </div>
+      <Formik
+        innerRef={formRef}
+        enableReinitialize={true}
+        initialValues={{
+          name: '',
+          email: '',
+          phone: '',
+          message: '',
+        }}
+        validationSchema={InvoiceFormSchema}
+        onSubmit={async (values: TContactForm, actions) => {
 
-        <div className="mt-10 text-center md:text-right md:mt-6">
-          <div className="inline-block">
-            <ButtonLight label="Submit" onClick={null}/>
-          </div>
-        </div>
-      </div>
+        }}>
+
+        {(formProps: FormikProps<TContactForm>) => {
+          const {
+            values,
+            touched,
+            errors,
+            handleBlur,
+            handleChange,
+          } = formProps;
+
+          return (
+            <Form className="mt-12 md:mt-10 lg:mt-0 lg:flex-1">
+              <div className="space-y-6">
+                <div>
+                  <InputComp
+                    placeholder="Name"
+                    name={'name'}
+                    id={'name'}
+                    error={
+                      (errors.name && touched.name )
+                        ? errors.name
+                        : ''
+                    }
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                <div>
+                  <InputComp
+                    placeholder="Email Address"
+                    name={'email'}
+                    id={'email'}
+                    error={
+                      (errors.email && touched.email )
+                        ? errors.email
+                        : ''
+                    }
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                <div>
+                  <InputComp
+                    placeholder="Phone"
+                    name={'phone'}
+                    id={'phone'}
+                    error={
+                      (errors.phone && touched.phone )
+                        ? errors.phone
+                        : ''
+                    }
+                    value={values.phone}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                <div>
+                  <TextAreaComp
+                    rows={5}
+                    placeholder="Your Message"
+                    name={'message'}
+                    id={'message'}
+                    error={
+                      (errors.message && touched.message )
+                        ? errors.message
+                        : ''
+                    }
+                    value={values.message}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+              </div>
+
+              <div className="mt-10 text-center md:text-right md:mt-6">
+                <div className="inline-block">
+                  <ButtonLight label="Submit" onClick={() => {
+                    formRef.current.handleSubmit();
+                  }}/>
+                </div>
+              </div>
+            </Form>
+          )
+        }}
+      </Formik>
     </div>
   )
 }
